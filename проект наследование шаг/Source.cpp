60e7862da10426185e55ec24033673aed44fe8b6
Aleksandr <shura.gorodnichev@mail.ru>
2021-11-30 21:07:04 +0300

    Сохранить

--- проект наследование шаг/Source.cpp
+++ проект наследование шаг/Source.cpp
@@ -18,7 +18,7 @@ using namespace std;
 из фигур.*/
 
 class Shape {
-	//int lordinata = 0;
+	
 public:
 	Shape(){}
 	virtual void Show() = 0;
@@ -26,22 +26,18 @@ public:
 	virtual void Load(const char* part) = 0;
 };
 class Square : public Shape {   // квадрат
-	//char** arr;
 	int dlina;
 public:
 	Square(int a) : dlina(a){}
-	virtual void Show() {
-			char** arr = new char* [dlina];
+	virtual void Show() override final {
 			for (int i = 0; i < dlina; i++) {
-				arr[i] = new char[dlina];
 				for (int j = 0; j < dlina; j++) {
-					arr[i][j] = '*';
-					cout << arr[i][j] << " ";
+					cout <<"*" << " ";
 				} cout << endl;
 			}	
 		}
 	
-	 virtual void Save(const char* part) {
+	 virtual void Save(const char* part) override final {
 		 ofstream fail;
 		 fail.open(part, ios::app);
 		 char** arr = new char* [dlina];
@@ -55,24 +51,21 @@ public:
 		 fail.close();
 	 }
 
-     /*virtual void Load(const char* part) {
+     virtual void Load(const char* part) override final {
 		ifstream fail;
 		char line[128];
 		fail.open(part);
 		//fail >> *slov;
 		int i = 0;
 		fail.getline(line, 128);
-		
-		
 		fail.close();
-	}*/
+	}
 };
-class Rectangle : public Shape {
+class Rectangle : public Shape  {
 	int dlina;
 	int vusota;
 public:
-	Rectangle(int a, int b) : dlina(a),vusota(b){}
-
+	Rectangle(int a,int b) : dlina(a),vusota(b){}
 	virtual void Show() {
 		char** arr = new char* [dlina];
 		for (int i = 0; i < dlina; i++) {
@@ -100,13 +93,13 @@ public:
 
 
 int main() {
-	//Square one(5);
-	//one.Show();
-	//one.Save("kv.txt");
-	//one.Load("kv.txt");
-	Rectangle two (10, 5);
-	two.Show();
-	two.Save("kv.txt");
+	Square one(5);
+	one.Show();
+	one.Save("kv.txt");
+	one.Load("kv.txt");
+	//Rectangle two (5,10);
+	//two.Show();
+	//two.Save("kv.txt");
 	//two.Load("kv.txt");
 	
 
