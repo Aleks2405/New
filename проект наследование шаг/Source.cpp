18a77917a7cd8e8808988f8fc81ed7daa95a0887
Goro_sb71@stud.msk.itstep.org <Goro_sb71@stud.msk.itstep.org>
2021-12-06 17:35:38 +0300

    Исправили, методыклассы квадрат

--- проект наследование шаг/Source.cpp
+++ проект наследование шаг/Source.cpp
@@ -22,14 +22,15 @@ class Shape {
 public:
 	Shape(){}
 	virtual void Show() = 0;
-	virtual void Save(const char* part) = 0;
-	virtual void Load(const char* part) = 0;
+	virtual void Save() = 0;
+	virtual void Load() = 0;
 };
 class Square : public Shape {   // квадрат
 	int dlina;
 public:
+	Square() : Shape() {}
 	Square(int a) : dlina(a){}
-	virtual void Show() override final {
+	virtual void Show() {
 			for (int i = 0; i < dlina; i++) {
 				for (int j = 0; j < dlina; j++) {
 					cout <<"*" << " ";
@@ -37,68 +38,67 @@ public:
 			}	
 		}
 	
-	 virtual void Save(const char* part) override final {
+	 virtual void Save()  {
 		 ofstream fail;
-		 fail.open(part, ios::app);
-		 char** arr = new char* [dlina];
+		 fail.open("kv.txt", ios::app);
+		 //char** arr = new char* [dlina];
 		 for (int i = 0; i < dlina; i++) {
-			 arr[i] = new char[dlina];
+			// arr[i] = new char[dlina];
 			 for (int j = 0; j < dlina; j++) {
-				 arr[i][j] = '*';
-				 fail << arr[i][j] << " ";
+				// arr[i][j] = '*';
+				 fail << "*" << " ";
 			 } fail << endl;
 		 }
 		 fail.close();
 	 }
 
-     virtual void Load(const char* part) override final {
+     virtual void Load() {
 		ifstream fail;
 		char line[128];
-		fail.open(part);
+		fail.open("kv.txt");
 		//fail >> *slov;
 		int i = 0;
 		fail.getline(line, 128);
 		fail.close();
 	}
 };
-class Rectangle : public Shape  {
+class Rectangle : public Shape {
 	int dlina;
 	int vusota;
 public:
-	Rectangle(int a,int b) : dlina(a),vusota(b){}
-	virtual void Show() {
-		char** arr = new char* [dlina];
-		for (int i = 0; i < dlina; i++) {
-			arr[i] = new char[vusota];
-			for (int j = 0; j < vusota; j++) {
-				arr[i][j] = '*';
-				cout << arr[i][j] << " ";
-			} cout << endl;
+	Rectangle(int a, int b) : dlina(a), vusota(b) {}
+		virtual void Show() {
+			for (int i = 0; i < dlina; i++) {
+				for (int j = 0; j < vusota; j++) {
+					cout << "*" << " ";
+				} cout << endl;
+			}
 		}
-	}
-	virtual void Save(const char* part) {
-		ofstream fail;
-		fail.open(part, ios::app);
-		char** arr = new char* [dlina];
-		for (int i = 0; i < dlina; i++) {
-			arr[i] = new char[vusota];
-			for (int j = 0; j < vusota; j++) {
-				arr[i][j] = '*';
-				fail << arr[i][j] << " ";
-			} fail << endl;
+		virtual void Save() {
+			ofstream fail;
+			fail.open("kv.txt", ios::app);
+			char** arr = new char* [dlina];
+			for (int i = 0; i < dlina; i++) {
+				arr[i] = new char[vusota];
+				for (int j = 0; j < vusota; j++) {
+					arr[i][j] = '*';
+					fail << arr[i][j] << " ";
+				} fail << endl;
+			}
+			fail.close();
 		}
-		fail.close();
-	}
+	
 };
 
 
 int main() {
-	Square one(5);
-	one.Show();
-	one.Save("kv.txt");
-	one.Load("kv.txt");
-	//Rectangle two (5,10);
-	//two.Show();
+	//Square one(5);
+	//one.Show();
+	//cout << endl;
+	//one.Save("kv.txt");
+	//one.Load("kv.txt");
+	Rectangle two(5,10);
+	two.Show();
 	//two.Save("kv.txt");
 	//two.Load("kv.txt");
 	
